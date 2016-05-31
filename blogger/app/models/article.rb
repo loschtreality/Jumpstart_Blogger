313bc789ac862e558332d7ad7204fe3462aaa1f6
Michael Loschiavo <losch103@gmail.com>
2016-05-31 13:16:02 -0700

    Added a few gems

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
-  has_many :comments #Not sure weather to remove this
+  has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
     def tag_list
       self.tags.collect do |tag|
